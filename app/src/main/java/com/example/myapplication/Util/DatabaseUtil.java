201c7fec42494a54e9e14b89a2279fe89c70391d
FaZziX <55323109+AbdeFazza@users.noreply.github.com>
2024-06-08 19:40:38 +0100

    Update DatabaseUtil.java

--- app/src/main/java/com/example/myapplication/Util/DatabaseUtil.java
+++ app/src/main/java/com/example/myapplication/Util/DatabaseUtil.java
@@ -18,7 +18,7 @@ import java.sql.SQLException;
 import java.util.Map;
 
 public class DatabaseUtil {
-    private static final String URL = "Firebase_API_Key";
+    private static final String URL = "Firebase_URL";
 
     public static DatabaseReference connect() {
         try {
